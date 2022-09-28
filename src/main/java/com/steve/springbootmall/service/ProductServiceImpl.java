fa7adf34c37ffafd070550d00b4b8c2daa5deaca
DaderChiu <DaderChiu@S00A9009-3512>
2022-09-28 17:19:14 +0800

    商品功能 - 實作修改商品

--- src/main/java/com/steve/springbootmall/service/ProductServiceImpl.java
+++ src/main/java/com/steve/springbootmall/service/ProductServiceImpl.java
@@ -27,6 +27,13 @@ public class ProductServiceImpl implements ProductService{
 		
 	}
 
+	@Override
+	public void updateProduct(Integer productId, ProductRequest productRequest) {
+
+		productDao.updateProduct(productId, productRequest);
+		
+	}
+
 	
 	
 }
