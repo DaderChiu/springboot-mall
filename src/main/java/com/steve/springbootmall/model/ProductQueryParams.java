44e435fd83bf1aaa6f789a2eae76a55c0ac7068e
DaderChiu <DaderChiu@S00A9009-3512>
2022-09-30 13:56:43 +0800

    商品功能 - 實作查詢商品列表(排序)

--- src/main/java/com/steve/springbootmall/model/ProductQueryParams.java
+++ src/main/java/com/steve/springbootmall/model/ProductQueryParams.java
@@ -2,11 +2,26 @@ package com.steve.springbootmall.model;
 
 import com.steve.springbootmall.constant.ProductCategory;
 
+//如果傳遞多個參數，創建一個類別處理請求參數比較方便
 public class ProductQueryParams {
 
-	ProductCategory category;
-	String search;
+	private ProductCategory category;
+	private String search;
+	private String orderBy;
+	private String sort;
 	
+	public String getOrderBy() {
+		return orderBy;
+	}
+	public void setOrderBy(String orderBy) {
+		this.orderBy = orderBy;
+	}
+	public String getSort() {
+		return sort;
+	}
+	public void setSort(String sort) {
+		this.sort = sort;
+	}
 	public ProductCategory getCategory() {
 		return category;
 	}
