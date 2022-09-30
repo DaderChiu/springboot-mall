48ef4c1c12b46dbff309a0c6268934f5e397fe12
DaderChiu <DaderChiu@S00A9009-3512>
2022-09-30 16:20:06 +0800

    商品功能 - 實作查詢商品列表(分頁)

--- src/main/java/com/steve/springbootmall/model/ProductQueryParams.java
+++ src/main/java/com/steve/springbootmall/model/ProductQueryParams.java
@@ -9,7 +9,22 @@ public class ProductQueryParams {
 	private String search;
 	private String orderBy;
 	private String sort;
+	private Integer limit;
+	private Integer offset;
 	
+	
+	public Integer getLimit() {
+		return limit;
+	}
+	public void setLimit(Integer limit) {
+		this.limit = limit;
+	}
+	public Integer getOffset() {
+		return offset;
+	}
+	public void setOffset(Integer offset) {
+		this.offset = offset;
+	}
 	public String getOrderBy() {
 		return orderBy;
 	}
